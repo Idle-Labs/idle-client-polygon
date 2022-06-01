3d68b92b1e8014205064b1464c09641dddd152bf
Samster91 <samster91>
2022-06-01 18:58:57 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40d71017e78a59ae3e8933cc6496652a.js"
+  "/precache-manifest.545fa0216065b7a53621718216347adb.js"
 );
 
 workbox.clientsClaim();
